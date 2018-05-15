4a6fc3472e223a82a68dae7dbc0ea7797ece629c
richard.field@flywheel.nz <ubWA5QkicB85Z7rZDCmS>
2018-05-15 13:40:17 +1200

    testing without using the template

--- src/app/_components/recipe-card/recipe-card.component.spec.ts
+++ src/app/_components/recipe-card/recipe-card.component.spec.ts
@@ -1,13 +1,7 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RecipeCardComponent } from './recipe-card.component';
 
-import { HighlightedResultPipe } from '../../_pipes/highlighted-result/highlighted-result.pipe';
-import { CapitalizePipe } from '../../_pipes/capitalize/capitalize.pipe';
-import { TimeSincePipe } from '../../_pipes/time-since/time-since.pipe';
-import { RootDomainPipe } from '../../_pipes/root-domain/root-domain.pipe';
-
 const recipe = {
+  key: '$key',
   category: 'entree',
   defaultThumbnailUrl: 'https://storage.googleapis.com/ketohub-gcs1/ruled-me_keto-tuna-casserole-680w.jpg',
   ingredients: ['butter', 'carrots'],
@@ -18,28 +12,81 @@ const recipe = {
   url: 'https://www.ruled.me/keto-tuna-casserole/',
 };
 
-describe('RecipeCardComponent', () => {
-  let component: RecipeCardComponent;
-  let fixture: ComponentFixture<RecipeCardComponent>;
+// Not necessary to create the template to test this component
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [RecipeCardComponent, HighlightedResultPipe, CapitalizePipe, TimeSincePipe, RootDomainPipe],
-    })
-    .compileComponents();
-  }));
+describe('RecipeCardComponent', () => {
+
+  let component: RecipeCardComponent;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RecipeCardComponent);
-    component = fixture.componentInstance;
-
-    component.recipe = recipe;
-    component.searchKeywords = null;
-
-    fixture.detectChanges();
+    component = new RecipeCardComponent();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // use the naming convention from The Art of Unit Testing by Roy Overshoe
+  // see also: http://osherove.com/blog/2005/4/3/naming-standards-for-unit-tests.html
+  // [UnitOfWork_StateUnderTest_ExpectedBehavior]
+  it('matchingIngredients_no ingredients_returns empty array', () => {
+    const result = component.matchingIngredients(null);
+    expect(result).toEqual([]);
+  });
+
+  it('matchingIngredients_no searchKeywords_returns empty array', () => {
+    component.searchKeywords = undefined;
+    const result = component.matchingIngredients([]);
+    expect(result).toEqual([]);
+  });
+
+  it('matchingIngredients_ingredients contains no keywords_returns empty array', () => {
+    component.searchKeywords = ['banana'];
+    const result = component.matchingIngredients(['bacon']);
+    expect(result).toEqual([]);
+  });
+
+  it('matchingIngredients_ingredients contains one keyword_returns match', () => {
+    component.searchKeywords = ['bacon'];
+    const result = component.matchingIngredients(['bacon', 'avocado']);
+    expect(result).toEqual(['bacon']);
+  });
+
+  it('getCardClass_searchKeywords undefined_returns empty string', () => {
+    component.searchKeywords = undefined;
+    const result = component.getCardClass();
+    expect(result).toEqual('');
+  });
+
+  it('getCardClass_searchKeywords empty_returns empty string', () => {
+    component.searchKeywords = [];
+    const result = component.getCardClass();
+    expect(result).toEqual('');
+  });
+
+  it('getCardClass_searchKeywords present_returns tall-card', () => {
+    component.searchKeywords = ['bacon'];
+    const result = component.getCardClass();
+    expect(result).toEqual('tall-card');
+  });
+
+  it('getDefaultThumbnailUrl_recipt present__returns correct url', () => {
+    const GCS_BUCKET: string = 'ketohub-gcs1';
+    component.recipe = recipe;
+    const expectedResult = `https://storage.googleapis.com/${GCS_BUCKET}/${recipe.key}-680w.jpg`
+    const result = component.getDefaultThumbnailUrl();
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('getThumbnailUrls_recipt present__returns correct urls', () => {
+    const GCS_BUCKET: string = 'ketohub-gcs1';
+    component.recipe = recipe;
+    const expectedThumbUrl680 = `https://storage.googleapis.com/${GCS_BUCKET}/${recipe.key}-680w.jpg 680w`
+    const expectedThumbUrl560 = `https://storage.googleapis.com/${GCS_BUCKET}/${recipe.key}-560w.jpg 560w`
+    const expectedThumbUrl340 = `https://storage.googleapis.com/${GCS_BUCKET}/${recipe.key}-340w.jpg 340w`
+    const result = component.getThumbnailUrls();
+    expect(result.split(',')[0].trim()).toEqual(expectedThumbUrl680);
+    expect(result.split(',')[1].trim()).toEqual(expectedThumbUrl560);
+    expect(result.split(',')[2].trim()).toEqual(expectedThumbUrl340);
   });
 });
